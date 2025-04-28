09aed12df1ca00e05098e58d908daaf9a9a0eafc
Bhupendra Kushwah <bhupenkushwah23@gmail.com>
2025-04-28 12:33:19 +0530

    fix css

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,23 +1,12 @@
 import React, { useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./TodoApp.css";
-import { useState } from "react";
-const TodoApp = ({ list, onNewItem, handleDelete }) => {
-  // const [todoItem, setTodoItem] = useState("");
-  // const [date, setDate] = useState("");
 
+const TodoApp = ({ list, onNewItem, handleDelete }) => {
   const todoNameElement = useRef();
   const DateElement = useRef();
 
-  // const handleAddItem = (event) => {
-  //   setTodoItem(event.target.value);
-
-  // };
-  // const handleAddDate = (event) => {
-  //   setDate(event.target.value);
-
-  // };
-  const handleOnLick = (event) => {
+  const handleOnClick = (event) => {
     event.preventDefault();
     const todoItem = todoNameElement.current.value;
     const date = DateElement.current.value;
@@ -27,57 +16,41 @@ const TodoApp = ({ list, onNewItem, handleDelete }) => {
       alert("Please enter task name and date!");
       return;
     }
-
     onNewItem(todoItem, date);
-    // setTodoItem("");
-    // setDate("");
   };
 
   return (
     <>
       <center className="fw-bolder">Todo App</center>
-      <br />
       <div className="todo-container container">
-        <div className="todo-input-group">
+        <form className="todo-input-group" onSubmit={handleOnClick}>
           <input
             type="text"
             placeholder="Enter Todo Task"
             className="todo-input"
-            // onChange={handleAddItem}
-            // value={todoItem}
             ref={todoNameElement}
           />
-          <input
-            type="date"
-            className="todo-date"
-            // value={date}
-            // onChange={handleAddDate}
-            ref={DateElement}
-          />
-
-          <button className="btn btn-success todo-btn " onClick={handleOnLick}>
+          <input type="date" className="todo-date" ref={DateElement} />
+          <button type="submit" className="btn btn-success todo-btn">
             Add
           </button>
-        </div>
-        <div>
-          <div className="">
-            {list.map((todo, index) => (
-              <div key={index} className="row flex ">
-                <div className="col-5">{todo.name}</div>
-                <div className="col-5">{todo.date}</div>
-                <div className="col-2">
-                  <button
-                    className="btn btn-danger todo-Date"
-                    onClick={() => {
-                      handleDelete(todo.name);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
+        </form>
+
+        <div className="mt-3">
+          {list.map((todo, index) => (
+            <div key={index} className="todo-list-item">
+              <div className="todo-task">{todo.name}</div>
+              <div className="todo-date-text">{todo.date}</div>
+              <div className="todo-actions">
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleDelete(todo.name)}
+                >
+                  Delete
+                </button>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
